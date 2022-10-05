a2f8ca37f2d9ddf78145e6893da5ad5e98456015
blaxxun <blaxxun@blaxxun.blaxxun>
2022-10-06 01:18:20 +0200

    crossplay update

--- TimedItemDestruction/Properties/AssemblyInfo.cs
+++ TimedItemDestruction/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TimedItemDestruction")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://valheim.thunderstore.io/package/Smoothbrain/TimedItemDestruction")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TimedItemDestruction")]
